8e77893b2db65bc55ee065a158e53504062e140b
lesnikovski <lesnikovski@gmail.com>
2026-05-10 09:10:26 +0300

    Implement UART setup

--- 002UARTExcercise/Core/Src/main.c
+++ 002UARTExcercise/Core/Src/main.c
@@ -1,14 +1,43 @@
+#include "stm32l476xx.h"
 #include "stm32l4xx_hal.h"
 
 void SystemClockConfig(void);
+void UART2_Init(void);
+void Error_Handler(void);
+
+UART_HandleTypeDef huart2;
+
+char *user_data = "The application is running\r\n";
 
 int main(void) {
 	HAL_Init();
 	SystemClockConfig();
+
+	UART2_Init();
+
+	HAL_UART_Transmit(&huart2, &user_data, length(), Timeout);
 
 	return 0;
 }
 
 void SystemClockConfig(void) {
 	// TODO: Use it later
+}
+
+void UART2_Init(void) {
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+
+	if (HAL_UART_Init(&huart2) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+void Error_Handler(void) {
+	// blink red LED
 }
