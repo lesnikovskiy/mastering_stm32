fa9a6e21c6b53b3ce4e4795c0178738c37cbd65e
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2026-05-12 16:19:38 +0300

    Add project for UART Interrupt

---
+++ 003UARTITExcercise/Core/Src/main.c
@@ -0,0 +1,61 @@
+#include "stm32l476xx.h"
+#include "stm32l4xx_hal.h"
+#include <string.h>
+
+#define MAX_BUFFER_SIZE (100UL)
+
+void SystemClockConfig(void);
+void UART2_Init(void);
+void Error_Handler(void);
+void convert_buffer_to_capital(uint8_t *pData, uint32_t size);
+
+UART_HandleTypeDef huart2;
+
+char *user_data = "The application is running\r\n";
+
+int main(void) {
+	HAL_Init();
+	SystemClockConfig();
+
+	UART2_Init();
+
+	HAL_UART_Transmit(&huart2, (uint8_t*) user_data, strlen(user_data), HAL_MAX_DELAY);
+
+	while (1);
+
+	return 0;
+}
+
+void SystemClockConfig(void) {
+	// TODO: Use it later
+}
+
+void UART2_Init(void) {
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+
+	if (HAL_UART_Init(&huart2) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+void Error_Handler(void) {
+	// blink red LED
+}
+
+void convert_buffer_to_capital(uint8_t *pData, uint32_t size) {
+	if (pData == NULL)
+		return;
+
+	for (uint32_t i = 0; i < size; i++) {
+		if (pData[i] >= 'a' && pData[i] <= 'z') {
+			pData[i] -= ('a' - 'A');
+		}
+	}
+}
